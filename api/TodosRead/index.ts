e81295f525132764570040325cd5fe85f2aec60f
smaru1111 <marusou20@gmail.com>
2023-07-19 17:14:42 +0900

    update swa hello world

--- api/TodosRead/index.ts
+++ api/TodosRead/index.ts
@@ -1,9 +1,12 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { Context, HttpRequest } from "@azure/functions"
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.res.json({
-        text: "Hello from the API"
-    });
-};
+const httpTrigger = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Hello from TodosRead!"
+    };
+}
 
 export default httpTrigger;
